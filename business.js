2bb3f26bdda56db9219da65a779b0eed41318cea
Jada Brown <jadarae.brown@mymona.uwi.edu>
2018-11-17 18:14:21 +0000

    original

---
+++ business.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("last");
+    if (x.style.display === "none") {
+        x.style.display = "grid";
+        
+    } else {
+        x.style.display = "none";
+    }
+};
